bd67d3d05f4c39b4eca5259a22eafa9de4e8bd10
Abhineswari <seela031@umn.edu>
2026-08-29 15:37:44 -0500

    Add double quotes to shell variable dereferencing
    This fixes all the double quopte warnings from shellcheck and leaves us
    with a clean file.

--- count_successes.sh
+++ count_successes.sh
@@ -9,11 +9,11 @@ tar_file=$1
 
 SCRATCH=$(mktemp --directory)
 
-tar zxf $tar_file --directory $SCRATCH
+tar zxf "$tar_file" --directory "$SCRATCH"
 
-num_successes=$(grep -lr SUCCESS $SCRATCH | wc -l)
+num_successes=$(grep -lr SUCCESS "$SCRATCH" | wc -l)
 
-num_failures=$(grep -lr FAILURE $SCRATCH | wc -l)
+num_failures=$(grep -lr FAILURE "$SCRATCH" | wc -l)
 
 echo "There were $num_successes successes and $num_failures failures." 
 
