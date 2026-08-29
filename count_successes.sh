751bda9ee27fe974d0c01a5bb51cda217a705cf0
Abhineswari <seela031@umn.edu>
2026-08-29 15:30:33 -0500

    Replace all backticks with `$(..._) construct
    This replaces all the occurrences of backtick with the safer and
    preferable `$(...)` construct.

--- count_successes.sh
+++ count_successes.sh
@@ -7,14 +7,14 @@
 
 tar_file=$1
 
-SCRATCH=`mktemp --directory`
+SCRATCH=$(mktemp --directory)
 
 tar zxf $tar_file --directory $SCRATCH
 
-num_successes=`grep -lr SUCCESS $SCRATCH | wc -l`
+num_successes=$(grep -lr SUCCESS $SCRATCH | wc -l)
 
-num_failures=`grep -lr FAILURE $SCRATCH | wc -l`
+num_failures=$(grep -lr FAILURE $SCRATCH | wc -l)
 
 echo "There were $num_successes successes and $num_failures failures." 
 
-rm -rf $SCRATCH
+rm -rf "$SCRATCH"
