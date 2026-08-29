cd4f164c491fea5a95ee9a45f2b7b1492476b674
Abhineswari <seela031@umn.edu>
2026-08-29 15:12:09 -0500

    Got the `count_success.sh` script working.

--- count_successes.sh
+++ count_successes.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/bash
 
-echo "There were 78 successes and 22 failures." 
+# Extracts contents of a compressed tar archived specified as
+# the first command line argument and count the number of files
+# that contain the word "SUCCESS" and the number containing the word
+# "FAILURE" and reports that result.
+
+tar_file=$1
+
+SCRATCH=`mktemp --directory`
+
+tar zxf $tar_file --directory $SCRATCH
+
+num_successes=`grep -lr SUCCESS $SCRATCH | wc -l`
+
+num_failures=`grep -lr FAILURE $SCRATCH | wc -l`
+
+echo "There were $num_successes successes and $num_failures failures." 
+
+rm -rf $SCRATCH
